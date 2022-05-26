0511388785d82cd6205c3dc4f8ec15485711094c
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-05-26 12:43:17 +0100

    Update imageslider

--- src/Imageslider.js
+++ src/Imageslider.js
@@ -8,16 +8,24 @@ const Imageslider= ({slides}) =>{
 const [current, setCurrent] = useState(0);
   const length = slides.length;
 
+  const nextSlide = () => {
+      setCurrent(current === length -1 ? 0 : current +1);
+  };
 
+  console.log (current );
+
+if(!Array.isArray (slides)|| slides.length <= 0){
+    return null;
+}
 
 
 
 return (
   <section className="slider">
-    <FaArrowAltCircleLeft className="leftarrow"  />
-    <FaArrowAltCircleRight className="rightarrow"  />
+    <FaArrowAltCircleLeft className="leftarrow" />
+    <FaArrowAltCircleRight className="rightarrow" onClick={nextSlide} />
     {Sliderdata.map((slide, index) => {
-      return <img src={slide.image} alt="imagescol" className="image"/>;
+      return <img src={slide.image} alt="imagescol" className="image" />;
     })}
   </section>
 );
