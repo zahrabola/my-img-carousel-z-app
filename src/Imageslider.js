2b3c1d80dde11503068d74be6a62838b3ce2f027
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-05-26 12:27:24 +0100

    update image slider

--- src/Imageslider.js
+++ src/Imageslider.js
@@ -1,17 +1,26 @@
 import React, { useState } from "react";
 import { Sliderdata } from "./Sliderdata";
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
+
 
 
 const Imageslider= () =>{
+const [current, setCurrent] = useState(0);
+  const length = slides.length;
+
+
+
+
+
 return (
-
-<>
-{Sliderdata.map((slide, index) => {
-    return <img src={slide.image} alt="images" />;
-})}
-</>
-
-)
+  <section>
+    <FaArrowAltCircleLeft className="left-arrow"  />
+    <FaArrowAltCircleRight className="right-arrow" />
+    {Sliderdata.map((slide, index) => {
+      return <img src={slide.image} alt="images" />;
+    })}
+  </section>
+);
 }
 
 
